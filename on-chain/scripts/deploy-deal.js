1764e9bd97893eb36c2fe605ed8c3d427ccb9d34
yiwengao <yiwen.gao@columbia.edu>
2023-02-04 11:48:27 -0800

    Update deploy scripts

--- on-chain/scripts/deploy-deal.js
+++ on-chain/scripts/deploy-deal.js
@@ -1,17 +1,42 @@
 const { ethers } = require("hardhat");
 
-async function main(clientAddress, nodeSchedule, dealDuration) {
+async function main(
+    userAddress, 
+    dealDuration,
+    hourlySegmentReward, 
+    totalFinalReward,
+    dailySchedule,
+    verifier,
+    poRep,
+) {
     const [deployer] = await ethers.getSigners();
     console.log("Deployer address:", deployer.address);
     console.log("Account balance:", (await deployer.getBalance()).toString());
     const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, ethers.provider);
     
     const StorageDeal = await ethers.getContractFactory("StorageDeal", wallet);
-    const deal = await StorageDeal.deploy(clientAddress, nodeSchedule, dealDuration);
+    const deal = await StorageDeal.deploy(
+        userAddress,  
+        dealDuration,
+        hourlySegmentReward, 
+        totalFinalReward,
+        dailySchedule,
+        verifier,
+        poRep,
+    );
     console.log("Contract address:", deal.address);
 }
 
-main()
+// NOTE replace the addresses with your own.
+main(
+    "0xbcb8E197F783E2aE4B3f3b6358B582a9692f9F85",
+    5,
+    ethers.utils.parseEther("1"),
+    ethers.utils.parseEther("1").mul(10),
+    [...Array(24).keys()].map(() => ["0xbcb8E197F783E2aE4B3f3b6358B582a9692f9F85"]),
+    "0x4c64aCa843786FA0FD9E90a94112435F9C56F41F",
+    "",
+)
     .then(() => process.exit(0))
     .catch((error) => {
         console.error(error);
